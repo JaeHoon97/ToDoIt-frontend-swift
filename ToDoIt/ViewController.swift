498b74c003318bcd5c9de8f458a2e99ecc6a48cc
hoon <stitch8971@gachon.ac.kr>
2024-05-23 02:46:17 +0900

    Initial Commit

---
+++ ToDoIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoIt
+//
+//  Created by 재훈 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
