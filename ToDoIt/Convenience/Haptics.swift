184c4533576a27a36d5bb65363093f0aecd04997
hoon <stitch8971@gachon.ac.kr>
2024-06-03 03:20:27 +0900

    ✨ Daily ToDo 리스트 기능 구현 ToDo 화면에서 오늘 날짜 표시 ToDo 화면에서 전체 일정 수, 완료 일정 수, 진행률 표시 Close : #20

--- ToDoIt/Convenience/Haptics.swift
+++ ToDoIt/Convenience/Haptics.swift
@@ -4,20 +4,23 @@
 //
 //  Created by 재훈 on 5/29/24.
 //
-
+// MARK: - 사용자의 화면 터치를 감지하고 진동을 발생
+ 
 import UIKit
 
 final class Haptics {
+    
     static let Shared = Haptics()
+    
     private init() { }
     
-    let lightHapticStyle = UIImpactFeedbackGenerator.FeedbackStyle.light
+    let lightHapticStyle = UIImpactFeedbackGenerator.FeedbackStyle.light // 진동의 세기를 light로 설정
     
-    lazy var lightHapticGenerator = UIImpactFeedbackGenerator(style: lightHapticStyle)
+    lazy var lightHapticGenerator = UIImpactFeedbackGenerator(style: lightHapticStyle) // 진동을 발생시키는 Generator 생성
     
     func generateLightHaptics() {
-        lightHapticGenerator.prepare()
-        lightHapticGenerator.impactOccurred()
+        lightHapticGenerator.prepare() // 진동 준비
+        lightHapticGenerator.impactOccurred() // 진동 발생
     }
 
 }
